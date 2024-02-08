4ce9081f393ccbbc3971dd028111303a943d10d6
Jonathan Verstraten <Jonathan.verstraten@stater.nl>
2024-02-08 14:33:49 +0100

    add prefetch to launches api

--- src/app/client-layout.tsx
+++ src/app/client-layout.tsx
@@ -1,14 +1,20 @@
 'use client';
 
 import type { NextPage } from 'next';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { QueryClient, HydrationBoundary } from '@tanstack/react-query';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { useState } from 'react';
 
-type Props = { children: React.ReactNode };
+type Props = { children: React.ReactNode; deHydratedState: unknown };
 
-const LaunchDetail: NextPage<Props> = ({ children }) => {
-  const queryClient = new QueryClient();
+const LaunchDetail: NextPage<Props> = ({ children, deHydratedState }) => {
+  const [queryClient] = useState(() => new QueryClient({ defaultOptions: { queries: { refetchOnWindowFocus: false, staleTime: 6 * 1000 } } }));
 
-  return <QueryClientProvider client={queryClient}>{children} </QueryClientProvider>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <HydrationBoundary state={deHydratedState}>{children}</HydrationBoundary>
+    </QueryClientProvider>
+  );
 };
 
 export default LaunchDetail;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,29 @@
 import type { Metadata } from 'next';
 import './globals.css';
+import { dehydrate, QueryClient } from '@tanstack/react-query';
 import ClientLayout from './client-layout';
 import Header from 'components/Layout/Header/Header';
+import { getLaunches } from 'utils/api/launches/launches';
 
 export const metadata: Metadata = {
   title: 'Rocket launch',
   description: 'Created for assessment purposes',
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const queryClient = new QueryClient();
+  await queryClient.prefetchQuery({ queryKey: ['launches'], queryFn: getLaunches });
+  const deHydratedState = dehydrate(queryClient);
+
   return (
     <html lang="en">
       <body>
         <Header navItems={[{ name: 'Home', path: '/' }]} />
-        <ClientLayout>{children}</ClientLayout>
+        <ClientLayout deHydratedState={deHydratedState}>{children}</ClientLayout>
       </body>
     </html>
   );
